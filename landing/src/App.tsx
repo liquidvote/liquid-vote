7ffe986a1a27042a0280c55a3cd47e115eacc517
goncalogomes <gonndr@gmail.com>
2021-05-30 23:54:19 +0100

    create landing page

---
+++ landing/src/App.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import loadable from "@loadable/component";
+
+import twitterIcon from "@assets/twitter.svg";
+
+export default function App() {
+  const themeFromParams = ((q) => q.get("theme") || "3")(
+    new URLSearchParams(window.location.search)
+  );
+
+  return (
+    <div className={`AppContainer theme${themeFromParams}`}>
+      <div className="row">
+        <div className="col-12">
+          <p>Webpack + TypeScript + React + SASS + Bootstrap = ❤️</p>
+
+          <hr />
+
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/Page1">Page1 - Env</Link>
+                </li>
+                <li>
+                  <Link to="/Page2">Page2 - Counter</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route
+                path="/Page1"
+                component={loadable(() => import("./components/Routes/Page1"))}
+              />
+              <Route
+                path="/Page2"
+                component={loadable(() => import("./components/Routes/Page2"))}
+              />
+            </Switch>
+          </Router>
+
+          <hr />
+
+          <a
+            className="btn btn-sm"
+            href="https://twitter.com/intent/tweet?text=@esperancaJs+halp"
+          >
+            <img className="img-fluid" src={twitterIcon} />
+            Any questions?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
