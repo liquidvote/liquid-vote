af25cbf77826540b4ad0db467861d5a5d3e96738
goncalogomes <gonndr@gmail.com>
2021-10-08 19:10:32 +0100

    fixed sidebar

--- app/src/components/shared/SideMenu/SideMenu.component.tsx
+++ app/src/components/shared/SideMenu/SideMenu.component.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useQuery } from "@apollo/client";
@@ -12,122 +12,146 @@ import GroupSvg from "@shared/Icons/Group.svg";
 import LoginIcon from "@shared/Icons/LoginIcon.svg";
 import Popper from "@shared/Popper";
 import useSearchParams from "@state/Global/useSearchParams.effect";
-import useAuthUser from '@state/AuthUser/authUser.effect';
+import useAuthUser from "@state/AuthUser/authUser.effect";
 
-import './style.sass';
+import "./style.sass";
 
-export const SideMenu: FunctionComponent<{}> = ({ }) => {
+export const SideMenu: FunctionComponent<{}> = ({}) => {
+  const { allSearchParams, updateParams } = useSearchParams();
 
-    const { allSearchParams, updateParams } = useSearchParams();
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    logout,
+    loginWithPopup,
+  } = useAuth0();
 
-    const { user, isAuthenticated, isLoading, loginWithRedirect, logout, loginWithPopup } = useAuth0();
+  const { liquidUser, liquidUser_refetch } = useAuthUser();
 
-    const { liquidUser, liquidUser_refetch } = useAuthUser();
+  useEffect(() => {
+    if (!!isAuthenticated) {
+      let count = 0;
+      const tryToGetUser = async () => {
+        const g = await liquidUser_refetch();
+        count = count + 1;
 
-    useEffect(() => {
-        if (!!isAuthenticated) {
+        // console.log({ count, g });
 
-            let count = 0;
-            const tryToGetUser = async () => {
-                const g = await liquidUser_refetch();
-                count = count + 1;
-
-                // console.log({ count, g });
-
-                if (!g?.data?.authUser) {
-                    setTimeout(() => tryToGetUser(), 100 + (count * 50));
-                }
-                
-            };
-            tryToGetUser();
+        if (!g?.data?.authUser) {
+          setTimeout(() => tryToGetUser(), 100 + count * 50);
         }
-    }, [isAuthenticated]);
+      };
+      tryToGetUser();
+    }
+  }, [isAuthenticated]);
 
-    return (
-        <div className="sideMenu">
-            <Link to="/home" className="hide-on-smaller-sideMenu">
-                <RippleDrop />
+  return (
+    <div className="side-container">
+      <div className="sideMenu">
+        <Link to="/home" className="hide-on-smaller-sideMenu">
+          <RippleDrop />
+        </Link>
+        <Link to="/home" data-tip="Home">
+          <HomeSvg />
+        </Link>
+        {isAuthenticated && user && (
+          <>
+            <Link to="/feed" data-tip="Notifications">
+              <NotificationSvg />
             </Link>
-            <Link to="/home" data-tip="Home">
-                <HomeSvg />
+            <Link to="/groups" data-tip="Your Groups">
+              <GroupSvg />
             </Link>
-            {isAuthenticated && user && (
-                <>
-                    <Link to="/feed" data-tip="Notifications">
-                        <NotificationSvg />
-                    </Link>
-                    <Link to="/groups" data-tip="Your Groups">
-                        <GroupSvg />
-                    </Link>
-                </>
-            )}
-            {/* <Link to="/feed">
+          </>
+        )}
+        {/* <Link to="/feed">
                 <BookmarkSvg />
             </Link>
             <Link to="/feed">
                 <AnalyticsSvg />
             </Link> */}
-            <div className="hide-on-smaller-sideMenu">
-                <br />
-                <br />
-                <br />
-                <br />
-            </div>
-            {isAuthenticated && user && (
-                <>
-                    <Popper
-                        rightOnSmall={true}
-                        button={<div>
-                            <img
-                                className="vote-avatar"
-                                src={liquidUser?.avatar || 'http://images.liquid-vote.com/system/loading.gif'}
-                                alt={liquidUser?.name || 'loading'}
-                            />
-                        </div>}
-                        popperContent={
-                            <ul className="p-0 m-0">
-                                <li>
-                                    {!!liquidUser ?
-                                        <Link to={`/profile/${liquidUser?.handle}`}>Visit Profile</Link> :
-                                        'loading...'
-                                    }
-                                </li>
-                                <li className="pointer" onClick={() => logout({
-                                    returnTo: window.location.origin
-                                })}>Logout</li>
-                            </ul>
-                        }
-                    />
-                    <div
-                        data-tip="Create Poll"
-                        className="button_ inverted icon-contain hide-on-smaller-sideMenu mt-3"
-                        onClick={() => updateParams({
-                            paramsToAdd: {
-                                modal: "EditQuestion",
-                                modalData: JSON.stringify({
-                                    questionHandle: 'new'
-                                })
-                            }
-                        })}
-                    >
-                        <DropPlusSVG />
-                    </div>
-                </>
-            )}
-            {!isAuthenticated && (
-                <>
-                    <div
-                        className="pointer"
-                        onClick={() => loginWithPopup({
-                            // redirectUri: window.location.origin
-                        })}
-                        data-tip="Login"
-                    >
-                        <LoginIcon />
-                    </div>
-                </>
-            )}
+        <div className="hide-on-smaller-sideMenu">
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
-    );
-}
-
+        {isAuthenticated && user && (
+          <>
+            <Popper
+              rightOnSmall={true}
+              button={
+                <div>
+                  <img
+                    className="vote-avatar"
+                    src={
+                      liquidUser?.avatar ||
+                      "http://images.liquid-vote.com/system/loading.gif"
+                    }
+                    alt={liquidUser?.name || "loading"}
+                  />
+                </div>
+              }
+              popperContent={
+                <ul className="p-0 m-0">
+                  <li>
+                    {!!liquidUser ? (
+                      <Link to={`/profile/${liquidUser?.handle}`}>
+                        Visit Profile
+                      </Link>
+                    ) : (
+                      "loading..."
+                    )}
+                  </li>
+                  <li
+                    className="pointer"
+                    onClick={() =>
+                      logout({
+                        returnTo: window.location.origin,
+                      })
+                    }
+                  >
+                    Logout
+                  </li>
+                </ul>
+              }
+            />
+            <div
+              data-tip="Create Poll"
+              className="button_ inverted icon-contain hide-on-smaller-sideMenu mt-3"
+              onClick={() =>
+                updateParams({
+                  paramsToAdd: {
+                    modal: "EditQuestion",
+                    modalData: JSON.stringify({
+                      questionHandle: "new",
+                    }),
+                  },
+                })
+              }
+            >
+              <DropPlusSVG />
+            </div>
+          </>
+        )}
+        {!isAuthenticated && (
+          <>
+            <div
+              className="pointer"
+              onClick={() =>
+                loginWithPopup({
+                  // redirectUri: window.location.origin
+                })
+              }
+              data-tip="Login"
+            >
+              <LoginIcon />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
