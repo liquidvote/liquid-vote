13bfc80ba885d81ebce8aaf2b924a8c1d94a8021
pedro <buesimples@gmail.com>
2022-09-06 09:40:52 +0100

    setting up sentry on the server

--- graphql-server/src/index-lambda.ts
+++ graphql-server/src/index-lambda.ts
@@ -1,9 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+const Sentry = require("@sentry/serverless");
+
+Sentry.AWSLambda.init({
+    dsn: "https://9af61052682a4b4c93b0c78026b22624@o1069562.ingest.sentry.io/6722028",
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
 
 import { configServer } from "./server";
 
 let counter = 0;
-exports.graphqlHandler = (event, context, callback) => {
+exports.graphqlHandler = Sentry.AWSLambda.wrapHandler((event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     counter++
     console.log(counter);
@@ -14,4 +23,4 @@ exports.graphqlHandler = (event, context, callback) => {
             credentials: true,
         },
     })).then((handler: any) => handler(event, context, callback));
-};
+});
